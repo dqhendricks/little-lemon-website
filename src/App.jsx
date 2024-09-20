82542caf03f1cafd3268a66fbfeafc6233f0f5f4
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-09-20 10:51:14 -0700

    added basename

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,44 @@ import Homepage from "./features/homepage/Homepage.jsx";
 import BookingPage from "./features/bookingpage/BookingPage.jsx";
 import BookingConfirmed from "./features/bookingpage/BookingConfirmed.jsx";
 
-const router = createBrowserRouter([
-  {
-    element: <PageLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Homepage />,
-      },
-      {
-        path: "/about",
-        element: <Homepage />,
-      },
-      {
-        path: "/menu",
-        element: <Homepage />,
-      },
-      {
-        path: "/reservations",
-        element: <BookingPage />,
-      },
-      {
-        path: "/reservation-confirmed",
-        element: <BookingConfirmed />,
-      },
-      {
-        path: "/order",
-        element: <Homepage />,
-      },
-      {
-        path: "/login",
-        element: <Homepage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <PageLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/about",
+          element: <Homepage />,
+        },
+        {
+          path: "/menu",
+          element: <Homepage />,
+        },
+        {
+          path: "/reservations",
+          element: <BookingPage />,
+        },
+        {
+          path: "/reservation-confirmed",
+          element: <BookingConfirmed />,
+        },
+        {
+          path: "/order",
+          element: <Homepage />,
+        },
+        {
+          path: "/login",
+          element: <Homepage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/capstone" }
+);
 
 function App() {
   return (
